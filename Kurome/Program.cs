936c3e87ae8486dbc7abcc141def57895d5300f1
Konstantinos Karakostas <konstantk@csd.auth.gr>
2021-06-04 09:40:01 +0300

    add periodic UDP broadcasting

--- Kurome/Program.cs
+++ Kurome/Program.cs
@@ -5,6 +5,7 @@ using System.Net;
 using System.Net.NetworkInformation;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using DokanNet;
 
@@ -12,47 +13,22 @@ namespace Kurome
 {
     class Program
     {
-        public static readonly string UDP_SUBNET = "235.132.20.12";
-        public static readonly int PORT = 33586;
+        
 
         static void Main(string[] args)
         {
-            
-            var address = IPAddress.Parse(UDP_SUBNET);
-            var ipEndPoint = new IPEndPoint(address, PORT);
-            CastUdpInfo(address, ipEndPoint, GetLocalIpAddress());
-            KuromeTcpServer server = new KuromeTcpServer();
-            server.StartServer();
+            KuromeUdpCastServer udpCastServer = new KuromeUdpCastServer();
+            udpCastServer.PeriodicCastUdpInfo(TimeSpan.FromSeconds(5), CancellationToken.None);
+            // var address = IPAddress.Parse(UDP_SUBNET);
+            // var ipEndPoint = new IPEndPoint(address, PORT);
+            // CastUdpInfo(address, ipEndPoint, GetLocalIpAddress());
+            KuromeTcpServer tcpServer = new KuromeTcpServer();
+            tcpServer.StartServer();
             Console.Read();
         }
 
-        private static void CastUdpInfo(IPAddress address, IPEndPoint endpoint, IEnumerable<string> localIpAddresses)
-        {
-            foreach (var interfaceIp in localIpAddresses)
-            {
-                String message = "kurome:" + interfaceIp + ":" + Environment.MachineName;
-                var data = Encoding.Default.GetBytes(message);
-                using var udpClient = new UdpClient(AddressFamily.InterNetwork);
-                udpClient.Client.Bind(new IPEndPoint(IPAddress.Parse(interfaceIp), PORT));
-                udpClient.JoinMulticastGroup(address);
-                udpClient.Ttl = 32;
-                udpClient.Send(data, data.Length, endpoint);
-                Console.WriteLine("Broadcast: \"{0}\" to {1}", message, interfaceIp);
-                udpClient.Close();
-            }
-        }
+        
 
-        private static IEnumerable<string> GetLocalIpAddress()
-        {
-            var networkInterfaces = NetworkInterface.GetAllNetworkInterfaces();
-            return (from network in networkInterfaces
-                where network.OperationalStatus == OperationalStatus.Up
-                select network.GetIPProperties()
-                into properties
-                from address in properties.UnicastAddresses
-                where address.Address.AddressFamily == AddressFamily.InterNetwork
-                where !IPAddress.IsLoopback(address.Address)
-                select address.Address.ToString()).ToList();
-        }
+        
     }
 }
