bdec102cfd5e6efd7ffe87bf2891b9966d318df9
Noire001 <noiredev@protonmail.com>
2021-07-13 06:29:01 +0300

    fix json problem caused variable name change

--- Kurome/FileNode.cs
+++ Kurome/FileNode.cs
@@ -1,9 +1,14 @@
+using System.Text.Json.Serialization;
+
 namespace Kurome
 {
     public class FileNode
     {
+        [JsonPropertyName("fileName")]
         public string FileName { get; set; }
+        [JsonPropertyName("isDirectory")]
         public bool IsDirectory { get; set; }
+        [JsonPropertyName("size")]
         public long Size { get; set; }
     }
 }
