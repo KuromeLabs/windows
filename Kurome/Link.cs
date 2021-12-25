d5a7f2a35b291dc0633797e19d13d59371ac972c
Noire001 <noiredev@protonmail.com>
2021-12-25 02:13:42 +0200

    Link: add sending and receiving FlatBuffers

--- Kurome/Link.cs
+++ Kurome/Link.cs
@@ -1,10 +1,9 @@
 using System;
-using System.IO;
-using System.IO.Compression;
 using System.Linq;
 using System.Net.Sockets;
-using System.Text;
 using System.Threading.Tasks;
+using FlatBuffers;
+using kurome;
 
 namespace Kurome
 {
@@ -12,32 +11,15 @@ namespace Kurome
     {
         private readonly TcpClient _client;
         public bool IsDisposed = false;
-        private byte[] _writeBuffer = new byte[16384];
+        public readonly FlatBufferBuilder BufferBuilder = new(1024);
         private byte[] _readBuffer = new byte[16384];
+        private readonly byte[] _sizeBuffer = new byte[4];
 
         public Link(TcpClient client)
         {
             _client = client;
         }
 
-        public void WritePrefixedFileBuffer(byte[] buffer, string fileName, long offset)
-        {
-            var fileNameOffsetBytes = Encoding.UTF8.GetBytes($"{fileName.Replace('\\', '/')}:{offset.ToString()}:");
-            var size = buffer.Length + fileNameOffsetBytes.Length + 1;
-            var sizeBytes = BitConverter.GetBytes(size);
-            if (buffer.Length + fileNameOffsetBytes.Length + 5 > _writeBuffer.Length)
-                Array.Resize(ref _writeBuffer, buffer.Length + fileNameOffsetBytes.Length + 5);
-            Buffer.BlockCopy(sizeBytes, 0, _writeBuffer, 0, 4);
-            _writeBuffer[4] = Packets.ActionWriteFileBuffer;
-            Buffer.BlockCopy(fileNameOffsetBytes, 0, _writeBuffer, 5, fileNameOffsetBytes.Length);
-            Buffer.BlockCopy(buffer, 0, _writeBuffer, 5 + fileNameOffsetBytes.Length, buffer.Length);
-            _client.GetStream().Write(_writeBuffer, 0, size + 4);
-        }
-        public void WritePrefixed(byte[] buffer)
-        {
-            WriteToLinkBuffer(ref buffer);
-            _client.GetStream().Write(_writeBuffer, 0, buffer.Length + 4);
-        }
         public void WritePrefixed(byte buffer)
         {
             _client.GetStream().Write(BitConverter.GetBytes(1).Append(buffer).ToArray());
@@ -45,21 +27,11 @@ namespace Kurome
 
         public byte[] ReadFullPrefixed(int timeout)
         {
-            return ReadFullPrefixed(timeout, out _);
-        }
-        public byte[] ReadFullPrefixed(int timeout, out int size)
-        {
-            var sizeBuffer = new byte[4];
-            var readPrefixTask = Task.Run(() =>
-            {
-                var bytesRead = 0;
-                while (bytesRead != 4)
-                    bytesRead += _client.GetStream().Read(sizeBuffer, 0 + bytesRead, 4 - bytesRead);
-            });
+            var readPrefixTask = ReadPrefix();
             Task.WaitAny(readPrefixTask, Task.Delay(TimeSpan.FromSeconds(timeout)));
             if (!readPrefixTask.IsCompleted)
                 throw new TimeoutException();
-            size = BitConverter.ToInt32(sizeBuffer);
+            var size = BitConverter.ToInt32(_sizeBuffer);
             var bytesRead = 0;
             if (_readBuffer.Length < size)
                 Array.Resize(ref _readBuffer, size + 4);
@@ -70,22 +42,27 @@ namespace Kurome
             }
             return _readBuffer;
         }
-        
-        public string BufferToString(byte[] array, int size)
+
+        private async Task ReadPrefix()
         {
-            return Encoding.UTF8.GetString(array, 0, size);
+            var bytesRead = 0;
+            while (bytesRead != 4)
+                bytesRead += await _client.GetStream().ReadAsync(_sizeBuffer, 0 + bytesRead, 4 - bytesRead);
         }
 
 
-        private void WriteToLinkBuffer(ref byte[] buffer)
+        public void SendBuffer(ByteBuffer buffer)
         {
-            if (buffer.Length + 4 > _writeBuffer.Length)
-                Array.Resize(ref _writeBuffer, buffer.Length + 4);
-            var sizeBytes = BitConverter.GetBytes(buffer.Length);
-            Buffer.BlockCopy(sizeBytes, 0, _writeBuffer, 0, 4);
-            Buffer.BlockCopy(buffer, 0, _writeBuffer, 4, buffer.Length);
+            _client.GetStream().Write(buffer.ToSpan(buffer.Position, buffer.Length - buffer.Position));
         }
 
+        public Packet GetPacket()
+        {
+            ReadFullPrefixed(15);
+            var bb = new ByteBuffer(_readBuffer);
+            var packet = Packet.GetRootAsPacket(bb);
+            return packet;
+        }
         public void Dispose()
         {
             IsDisposed = true;
