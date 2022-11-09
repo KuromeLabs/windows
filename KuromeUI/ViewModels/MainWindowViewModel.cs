ce1da5ef0e0a4daae3e24c2f24364cff5cfe455b
Konstantinos Karakostas <noiredev@protonmail.com>
2022-11-09 12:30:51 +0200

    Create UI project

---
+++ KuromeUI/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,8 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+namespace KuromeUI.ViewModels
+{
+    public class MainWindowViewModel : ObservableObject
+    {
+        public string Greeting => "Welcome to Avalonia!";
+    }
+}
